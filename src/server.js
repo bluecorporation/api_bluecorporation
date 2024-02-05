fe9d3be4ffeb614240bacbd7b1ad8bf48159f241
Gustavo Henrique <moisescarvalhocfl7@gmail.com>
2024-02-05 15:15:49 -0300

    commit v3

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ require("dotenv").config();
 const app = require("./app");
 
 app.listen(3333,() => {
-    console.log("Server listen in port 4444")
+    console.log("Server listen in port 3333")
 })
