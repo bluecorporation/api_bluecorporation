d14a845255bd213133c18b194238860caf439d48
Gustavo Henrique <moisescarvalhocfl7@gmail.com>
2024-02-07 21:47:48 -0300

    v4

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ require("dotenv").config();
 const app = require("./app");
 
 app.listen(8888,() => {
-    console.log("Server listen in port 8888")
+    console.log("Server listen in port 4444")
 })
